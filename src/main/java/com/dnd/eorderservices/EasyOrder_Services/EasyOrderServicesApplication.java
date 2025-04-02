7e0ce5087c73b458d8ff5b71096e379e875da084
thisisjtaylor <thisisjtaylor@gmail.com>
2025-04-02 18:07:58 -0400

    first commit

---
+++ src/main/java/com/dnd/eorderservices/EasyOrder_Services/EasyOrderServicesApplication.java
@@ -0,0 +1,13 @@
+package com.dnd.eorderservices.EasyOrder_Services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyOrderServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyOrderServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnd/eorderservices/EasyOrder_Services/EasyOrderServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd.eorderservices.EasyOrder_Services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyOrderServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
